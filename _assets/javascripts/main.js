485b9bcdab0bf2e442efa0b3a9b979283f9a6ec0
ZePedroResende <j.p.resende68@gmail.com>
2018-03-04 18:16:13 +0000

    Site updated at 2018-03-04 18:16:13 UTC

--- _assets/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-//= require vendor/jquery.js
-//= require vendor/bootstrap.js
-
-//= require vendor/slick.js
-
-//= require nav-var.js
-//= require about-text.js
-//= require scrolldiv.js
-//= require fadein.js
